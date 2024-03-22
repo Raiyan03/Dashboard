f61a7651c0a5d5a5147e1893ace197014b619517
Ryan <ryansheikh003@gmail.com>
2024-03-22 02:28:32 -0600

    Added RightBar

--- app/ui/dashboard/rightbar/rightbar.jsx
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -1,9 +1,36 @@
+import {MdPlayCircleFilled, MdReadMore } from 'react-icons/md'
 import styles from './rightbar.module.css'
-
+import Image from 'next/image'
 export default function Rightbar() {
   return (
-    <div>
-      Rigthbar
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.bgContainer}>
+          <Image src="/astronaut.png" alt="backgroundImg" fill/>
+        </div>
+        <span className={styles.available}>🔥 Available Now</span>
+        <h3 className={styles.title}>How to use the new version of the admin dashboard?</h3>
+        <span className={styles.time}> Takes 4 minutes </span>
+        <p className={styles.description}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores dolorem 
+        </p>
+        <button className={styles.button}>
+          <MdPlayCircleFilled />
+          Watch
+        </button>
+      </div>
+
+      <div className={styles.item}>
+        <span className={styles.available}>🚀 Coming Soon</span>
+        <span className={styles.title}> New server actions are available, partial pre-rendering is coming u! </span>
+        <p className={styles.description}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores dolorem 
+        </p>
+        <button className={styles.button}>
+          <MdReadMore />
+          Learn
+        </button>
+      </div>
     </div>
   )
 }
